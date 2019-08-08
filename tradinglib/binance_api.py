d09f2ae8356122fe57b1309d98ff9d4d706c49e0
Deny <dl.rodrigues94@gmail.com>
2019-08-08 17:55:51 -0300

    Create withdraw

--- tradinglib/binance_api.py
+++ tradinglib/binance_api.py
@@ -37,8 +37,8 @@ class BinanceAPI(BaseAPI):
         return balances
 
     def list_orderbook(self, currency_price='USDT', currency_quantity='BTC', limit=10):
-        market = currency_quantity + currency_price
-        book = self._client.get_order_book(symbol=market)
+        symbol = currency_quantity + currency_price
+        book = self._client.get_order_book(symbol=symbol)
 
         book_buy_orders = book.get('bids')[:limit]
         book_buy_orders = [{
@@ -53,3 +53,35 @@ class BinanceAPI(BaseAPI):
         } for order in book_sell_orders]
 
         return book_buy_orders, book_sell_orders
+
+    def create_order(self, order_type: str, currency_price: str, currency_quantity: str,
+                     unit_price: Decimal = None, quantity: Decimal = None,
+                     execution_type: str = BaseAPI.ORDER_LIMIT) -> dict:
+        order = {}
+        symbol = currency_quantity + currency_price
+        params = {
+            'symbol': symbol,
+            'price': unit_price,
+            'recvWindow': self.recv_window,
+        }
+        if execution_type == self.ORDER_LIMIT:
+            if order_type == self.ORDER_BUY:
+                params['quantity'] = quantity
+                order = self._client.order_limit_buy(**params)
+            elif order_type == self.ORDER_SELL:
+                params['quantity'] = quantity
+                order = self._client.order_limit_sell(**params)
+        return order
+
+    def create_withdraw(self, currency: str, quantity: Decimal, address: str, tag: str = None) -> dict:
+        params = {
+            'asset': currency,
+            'amount': quantity,
+            'address': address,
+            'recvWindow': self.recv_window,
+        }
+        if tag:
+            params['addressTag'] = tag
+
+        withdraw = self._client.withdraw(**params)
+        return withdraw
